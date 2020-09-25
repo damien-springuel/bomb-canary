58b8c214d6615cda167fb1f22d3026c629205505
Damien Springuel <damien.springuel.1@gmail.com>
2020-09-25 14:44:57 -0600

    Remove player and start game

--- server/gamerules/game.go
+++ server/gamerules/game.go
@@ -1,15 +1,21 @@
 package gamerules
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
-	errPlayerNotFound = errors.New("player not found")
+	errPlayerNotFound        = errors.New("player not found")
+	errNotEnoughPlayers      = errors.New("not enough players")
+	errInvalidStateForAction = errors.New("invalid state for action")
 )
 
 type state string
 
 const (
-	notStarted state = "notStarted"
+	notStarted    state = "notStarted"
+	selectingTeam state = "selectingTeam"
 )
 
 type players []string
@@ -42,16 +48,38 @@ func newGame(id string) game {
 	return game{id: id, state: notStarted}
 }
 
-func (g game) addPlayer(name string) game {
+func (g game) addPlayer(name string) (game, error) {
+	if g.state != notStarted {
+		return g, fmt.Errorf("%w: can only add player during %s state, state was %s", errInvalidStateForAction, notStarted, g.state)
+	}
+
 	g.players = append(g.players, name)
-	return g
+	return g, nil
 }
 
 func (g game) removePlayer(name string) (game, error) {
+	if g.state != notStarted {
+		return g, fmt.Errorf("%w: can only remove player during %s state, state was %s", errInvalidStateForAction, notStarted, g.state)
+	}
+
 	p, err := g.players.remove(name)
 	if err != nil {
 		return g, err
 	}
+
 	g.players = p
+	return g, nil
+}
+
+func (g game) start() (game, error) {
+	if g.state != notStarted {
+		return g, fmt.Errorf("%w: can only start the game during %s state, state was %s", errInvalidStateForAction, notStarted, g.state)
+	}
+
+	if len(g.players) < 5 {
+		return g, errNotEnoughPlayers
+	}
+
+	g.state = selectingTeam
 	return g, nil
 }
--- server/gamerules/game_test.go
+++ server/gamerules/game_test.go
@@ -14,15 +14,31 @@ func Test_CreateGame(t *testing.T) {
 
 func Test_AddPlayer(t *testing.T) {
 	newGame := newGame("123")
-	newGame = newGame.addPlayer("Alice")
+	newGame, _ = newGame.addPlayer("Alice")
 
 	g := NewWithT(t)
 	g.Expect(newGame.players).To(Equal(players([]string{"Alice"})))
 }
 
+func Test_AddPlayer_ShouldErrorIfGameHasStarted(t *testing.T) {
+	newGame := newGame("123")
+	newGame, _ = newGame.addPlayer("Alice")
+	newGame, _ = newGame.addPlayer("Bob")
+	newGame, _ = newGame.addPlayer("Charlie")
+	newGame, _ = newGame.addPlayer("Dan")
+	newGame, _ = newGame.addPlayer("Edith")
+
+	newGame, _ = newGame.start()
+
+	newGame, err := newGame.addPlayer("Frank")
+
+	g := NewWithT(t)
+	g.Expect(err).To(MatchError(errInvalidStateForAction))
+}
+
 func Test_RemovePlayer(t *testing.T) {
 	newGame := newGame("123")
-	newGame = newGame.addPlayer("Alice")
+	newGame, _ = newGame.addPlayer("Alice")
 	newGame, err := newGame.removePlayer("Alice")
 
 	g := NewWithT(t)
@@ -30,12 +46,71 @@ func Test_RemovePlayer(t *testing.T) {
 	g.Expect(newGame.players).To(Equal(players([]string{})))
 }
 
-func Test_RemovePlayer_PlayerNotFound(t *testing.T) {
+func Test_RemovePlayer_ShouldErrorIfPlayerNotFound(t *testing.T) {
 	newGame := newGame("123")
-	newGame = newGame.addPlayer("Alice")
+	newGame, _ = newGame.addPlayer("Alice")
 	newGame, err := newGame.removePlayer("Bob")
 
 	g := NewWithT(t)
 	g.Expect(err).To(Equal(errPlayerNotFound))
 	g.Expect(newGame.players).To(Equal(players([]string{"Alice"})))
+}
+
+func Test_RemovePlayer_ShouldErrorIfGameHasStarted(t *testing.T) {
+	newGame := newGame("123")
+	newGame, _ = newGame.addPlayer("Alice")
+	newGame, _ = newGame.addPlayer("Bob")
+	newGame, _ = newGame.addPlayer("Charlie")
+	newGame, _ = newGame.addPlayer("Dan")
+	newGame, _ = newGame.addPlayer("Edith")
+
+	newGame, _ = newGame.start()
+
+	newGame, err := newGame.removePlayer("Bob")
+
+	g := NewWithT(t)
+	g.Expect(err).To(MatchError(errInvalidStateForAction))
+}
+
+func Test_StartGame_WhenFewerThan5Players_ShouldError(t *testing.T) {
+	newGame := newGame("123")
+	newGame, _ = newGame.addPlayer("Alice")
+	newGame, _ = newGame.addPlayer("Bob")
+	newGame, _ = newGame.addPlayer("Charlie")
+	newGame, _ = newGame.addPlayer("Dan")
+
+	newGame, err := newGame.start()
+
+	g := NewWithT(t)
+	g.Expect(err).To(Equal(errNotEnoughPlayers))
+}
+
+func Test_StartGame(t *testing.T) {
+	newGame := newGame("123")
+	newGame, _ = newGame.addPlayer("Alice")
+	newGame, _ = newGame.addPlayer("Bob")
+	newGame, _ = newGame.addPlayer("Charlie")
+	newGame, _ = newGame.addPlayer("Dan")
+	newGame, _ = newGame.addPlayer("Edith")
+
+	newGame, err := newGame.start()
+
+	g := NewWithT(t)
+	g.Expect(err).To(BeNil())
+	g.Expect(newGame.state).To(Equal(selectingTeam))
+}
+
+func Test_StartGame_ShouldErrorIfGameHasStarted(t *testing.T) {
+	newGame := newGame("123")
+	newGame, _ = newGame.addPlayer("Alice")
+	newGame, _ = newGame.addPlayer("Bob")
+	newGame, _ = newGame.addPlayer("Charlie")
+	newGame, _ = newGame.addPlayer("Dan")
+	newGame, _ = newGame.addPlayer("Edith")
+
+	newGame, _ = newGame.start()
+	newGame, err := newGame.start()
+
+	g := NewWithT(t)
+	g.Expect(err).To(MatchError(errInvalidStateForAction))
 }
