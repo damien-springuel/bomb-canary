8cc50f4bda77d707a1b5009131aeb52eacc7c87d
Damien Springuel <damien.springuel.1@gmail.com>
2020-10-14 20:17:12 -0600

    Install axios and use it to call create party endpoint

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+import Axios from "axios";
+
+let name: string;
+
+function createParty() {
+  Axios.post("http://localhost:44324/party/create", {name: name}, {withCredentials: true}).then(r => console.log(r));
+}
+
+</script>
+
 <div class="flex flex-col items-center h-full bg-gray-900 text-blue-500 px-6 overflow-auto">
   <h1 class="text-5xl">
     Bomb Canary
@@ -7,11 +18,11 @@
   </h2>
 
   <div class="flex-grow flex flex-col justify-evenly w-full text-2xl items-stretch space-y-10 mb-4">
-    <input type="text" placeholder="Name" class="bg-blue-400 focus:bg-blue-200 hover:bg-blue-300 placeholder-gray-700 rounded-lg px-2 text-gray-900 outline-none">
+    <input type="text" placeholder="Name" class="bg-blue-400 focus:bg-blue-200 hover:bg-blue-300 placeholder-gray-700 rounded-lg px-2 text-gray-900 outline-none" bind:value={name}>
     <div class="border-solid border-t-2 border-blue-500"></div>
     <input type="text" placeholder="Party Code" class="bg-blue-400 focus:bg-blue-200 hover:bg-blue-300 placeholder-gray-700 rounded-lg px-2 text-gray-900 outline-none">
     <button class="rounded-full px-6 py-4 bg-blue-500 hover:bg-blue-300 text-gray-900 focus:outline-none">Join Party</button>
     <div class="text-center">or</div>
-    <button class="rounded-full px-6 py-4 bg-blue-500 hover:bg-blue-300 text-gray-900 focus:outline-none">Create Party</button>
+    <button class="rounded-full px-6 py-4 bg-blue-500 hover:bg-blue-300 text-gray-900 focus:outline-none" on:click={createParty}>Create Party</button>
   </div>
 </div>
